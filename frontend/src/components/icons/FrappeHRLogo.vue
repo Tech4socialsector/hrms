b76da34946196ebc53e89b30164c08105be140b3
Nishanth Clinton A <nishanth.a@azimpremjifoundation.org>
2026-07-27 22:05:51 +0530

    update

--- frontend/src/components/icons/FrappeHRLogo.vue
+++ frontend/src/components/icons/FrappeHRLogo.vue
@@ -1,7 +1,11 @@
 <template>
 	<img
-		src="/assets/hrms/manifest/manifest-icon-512.maskable.png"
+		:src="logoUrl"
 		alt="Pathways"
 		class="rounded-lg object-contain"
 	/>
 </template>
+
+<script setup>
+const logoUrl = "/assets/hrms/manifest/manifest-icon-512.maskable.png"
+</script>
